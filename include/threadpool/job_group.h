c843e6fd67a573f71df820c664c7c32691d5a78b
Zehua-Chen <peterchen06242000@outlook.com>
2020-01-16 02:07:35 -0600

    [docs] Add documentation on job groups

--- include/threadpool/job_group.h
+++ include/threadpool/job_group.h
@@ -4,13 +4,30 @@
 #include <mutex>
 
 namespace threadpool {
+/**
+ * @brief A way to control a group of jobs
+ */
 class JobGroup {
  public:
+  /**
+   * @brief Create a new job group
+   */
   JobGroup();
 
+  /**
+   * @brief Notify the job group that a job has entered the job group
+   */
   void Enter();
+
+  /**
+   * @brief Notify the job group that a job has left the job group
+   */
   void Leave();
 
+  /**
+   * @brief Block until all jobs that have entered a job group has left the job
+   * group
+   */
   void Wait();
 
  private:
